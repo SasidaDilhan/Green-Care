2a9b80c4ff36d160de291dc44b5300a8f579c6b1
IT21158254 <IT21158254@my.sliit.lk>
2023-06-17 13:16:24 +0530

    working

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-function index() {
-  return <div>hello</div>;
-}
-
-export default index;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
