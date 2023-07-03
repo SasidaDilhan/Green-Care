5ea73b30601f9fa3a51687646fa758e22b7b7267
IT21158254 <IT21158254@my.sliit.lk>
2023-07-03 21:09:53 +0530

    building ui

--- FRONTEND/src/Home/Home.js
+++ FRONTEND/src/Home/Home.js
@@ -1,48 +1,13 @@
 import React from "react";
-import "./Home.css";
-import { Carousel } from "bootstrap";
-import img1 from "../Assests/1.jpg";
-import img2 from "../Assests/2.jpg";
-import img3 from "../Assests/3.jpg";
-import img4 from "../Assests/4.jpg";
+import Caraousel from "./Carosule.js";
+import NavBar from "./NavBar.js";
 
 function Home() {
   return (
-    <>
-      <div className="hero">
-        <div id="carouselExample" class="carousel slide">
-          <div class="carousel-inner">
-            <div class="carousel-item active ">
-              <img src={img1} class="img-fluid img" alt="..." />
-            </div>
-            <div class="carousel-item">
-              <img src={img2} class="img-fluid img" alt="..." />
-            </div>
-            <div class="carousel-item">
-              <img src={img3} class="img" alt="..." />
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-    </>
+    <div>
+      <NavBar />
+      <Caraousel />
+    </div>
   );
 }
 
