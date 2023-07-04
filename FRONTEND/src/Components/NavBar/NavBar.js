f97eb25ac77ba107a93365824b673a1a7c2d8b9d
IT21158254 <IT21158254@my.sliit.lk>
2023-07-04 13:33:27 +0530

    done

--- FRONTEND/src/Components/NavBar/NavBar.js
+++ FRONTEND/src/Components/NavBar/NavBar.js
@@ -1,20 +1,35 @@
 import { Component } from "react";
 import "./NavBarStyles.css";
+import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Green Care</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
 
-        <ul className="nav-menu">
-          <li>
-            <Link to="/">
-              <i className="fa-solid fa-house-chimney"></i>
-              Home
-            </Link>
-          </li>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li>
+                <Link to={item.url} className={item.cName}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
         </ul>
       </nav>
     );
